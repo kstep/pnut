9668ae10978669a1b0ab8e1c8a006a378e3c4809
Konstantin Stepanov <me@kstep.me>
2009-04-17 02:07:11 +0300

    Bugfix FS#5 - fileinfo couldn't find magic.mime
    I put path to magic file into finfo_open() function call.
    It's not nice, as this path is site/hoster specific, so it should
    be put in some kind of site-wide config (@todo).

--- compat.php
+++ compat.php
@@ -5,7 +5,7 @@ if (!function_exists('mime_content_type'))
 	{
 		function mime_content_type($filename)
 		{
-			$finfo = finfo_open(FILEINFO_MIME);
+			$finfo = finfo_open(FILEINFO_MIME, "/usr/share/file/magic");
 			$mime  = finfo_file($finfo, $filename);
 			finfo_close($finfo);
 			return $mime;
