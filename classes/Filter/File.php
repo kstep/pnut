2012758ccd12d003832d1d18d701fbd2aaf26b91
Konstantin Stepanov <me@kstep.me>
2009-04-24 17:56:05 +0300

    Production

--- classes/Filter/File.php
+++ classes/Filter/File.php
@@ -3,11 +3,11 @@ class Filter_File
 {
 	private static $_suffixes = array( "б", "Кб", "Мб", "Гб", "Тб" );
 
-	public static function humanizeSize($size)
+	public static function humanizeSize($size, $round = 3)
 	{
 		$maxpower = count(self::$_suffixes);
 		for ($i = 0; $size > 1024 && $i < $maxpower; $i++) $size /= 1024;
-		return sprintf("%0.3f %s", $size, self::$_suffixes[$i]);
+		return sprintf("%0.{$round}f %s", $size, self::$_suffixes[$i]);
 	}
 }
 ?>
