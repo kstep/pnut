fc9df73abb08e41d7151b5dbe4b79fc0f274a108
Konstantin Stepanov <me@kstep.me>
2009-05-01 14:53:21 +0300

    Added new filter class for text filtering
    Only one method is available for now:
    strip($text, $length, $hellip = '&hellip;', $side = Filter_Text::STRIP_RIGHT);
    
    If $text is longer than $length then its stripped up to $length and
    $hellip is placed instead of stripped text. It strips from side $side
    which can be STRIP_LEFT, STRIP_RIGHT, STRIP_CENTER or STRIP_BOTH.

---
+++ classes/Filter/Text.php
@@ -0,0 +1,34 @@
+<?php
+class Filter_Text
+{
+	const STRIP_LEFT = 0;
+	const STRIP_RIGHT = 1;
+	const STRIP_CENTER = 2;
+	const STRIP_BOTH = 3;
+
+	static public function strip($text, $length, $hellip = "&hellip;", $side = self::STRIP_RIGHT)
+	{
+		if (strlen($text) > $length)
+		{
+			switch ($side)
+			{
+				case self::STRIP_LEFT:
+					$text = substr($text, -$length) . $hellip;
+					break;
+				case self::STRIP_RIGHT:
+					$text = substr($text, 0, $length) . $hellip;
+					break;
+				case self::STRIP_CENTER:
+					$half = floor($length / 2);
+					$text = substr($text, 0, $half) . $hellip . substr($text, -$half);
+					break;
+				case self::STRIP_BOTH:
+					$half = floor((strlen($text) - $length) / 2);
+					$text = $hellip . substr($text, $half, $length) . $hellip;
+					break;
+			}
+		}
+		return $text;
+	}
+}
+?>
