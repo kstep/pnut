270c3d7e3c7b106a16558be55cd40881390dd43c
Konstantin Stepanov <me@kstep.me>
2009-05-03 02:17:02 +0300

    AP: fixed file view/download/thumbnails

---
+++ controllers/Admin/File.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package Site
+ * Controller for files processing
+ */
+class Controller_Admin_File extends Controller_Admin
+{
+    protected $_actions = array(
+        'download' => 'actionDownload',
+        'view'     => 'actionView',
+    );
+
+	private function sendFile(Model_Attachment $attach, $download = false)
+	{
+        $view = new View_Raw($attach->getFilepath());
+		$view->mimetype = $attach->mimetype;
+		$view->filename = $attach->filename;
+		$view->download = $download;
+		return $view;
+	}
+
+    public function actionDownload($params)
+    {
+        return $this->sendFile(new Model_Attachment($this->getStorage(), $params["id"]), true);
+    }
+
+    public function actionView($params)
+    {
+        return $this->sendFile(new Model_Attachment($this->getStorage(), $params["id"]), false);
+    }
+
+}
+?>
---
+++ controllers/Admin/Image.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @package Site
+ * Controller for image processing
+ */
+class Controller_Admin_Image extends Controller_Admin
+{
+    protected $_actions = array(
+        'thumbnail' => 'actionThumbnail',
+        'meta'      => 'actionMetadata',
+    );
+
+
+    private function imageNotFound()
+    {
+        throw new Controller_Exception("Image not found", 404, new View_Html());
+    }
+
+    public function actionThumbnail($params)
+    {
+        $attachment = new Model_Attachment($this->getStorage(), $params['id']);
+        if ($attachment->getId())
+        {
+			if ($attachment->isImage())
+			{
+				$file = $attachment->getFilepath();
+			}
+			elseif ($attachment->isVideo())
+			{
+				if (class_exists("ffmpeg_movie"))
+				{
+					$film = new ffmpeg_movie($attachment->getFilepath());
+					$frame = ceil($film->getFrameCount() * 0.1);
+					$file = $film->getFrame($frame)->toGDImage();
+				}
+				else
+				{
+					$this->imageNotFound();
+				}
+			}
+			else
+			{
+				$this->imageNotFound();
+			}
+
+			$view = new View_Image_Gd2($file);
+
+            if ($view)
+            {
+                $view->scale($params['scale']);
+            }
+            else
+            {
+                $this->imageNotFound();
+            }
+        }
+        else
+        {
+            $this->imageNotFound();
+        }
+        return $view;
+    }
+
+    public function actionMetadata($params)
+    {
+        $attachment = new Model_Attachment($this->getStorage(), $params['id']);
+        if ($attachment->getId())
+        {
+            $view = new View_Json();
+            $view->id   = $attachment->getId();
+            $view->info = $attachment->toArray();
+            if ($attachment->isImage() && $attachment->checkFile())
+                $view->image = getimagesize($attachment->getFilepath());
+        }
+        else
+        {
+            $this->imageNotFound();
+        }
+        return $view;
+    }
+}
+?>
